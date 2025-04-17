bf1ff50bbd11f1d94743d7362d5b4d58bb94d1a4
Markuss Broks <markuss.broks@gmail.com>
2025-04-16 20:13:18 -0700

    drivers: serial: earlycon: Correct argument name
    The "node" argument is actually an offset, and it's also
    an "int", and not "unsigned long". Correct the of_setup_earlycon
    function.
    
    Signed-off-by: Markuss Broks <markuss.broks@gmail.com>
    Reported-by: kernel test robot <lkp@intel.com>

--- include/linux/serial_core.h
+++ include/linux/serial_core.h
@@ -945,7 +945,7 @@ extern const struct earlycon_id __earlycon_table_end[];
 
 #define EARLYCON_DECLARE(_name, fn)	OF_EARLYCON_DECLARE(_name, "", fn)
 
-int of_setup_earlycon(const struct earlycon_id *match, unsigned long node,
+int of_setup_earlycon(const struct earlycon_id *match, int offset,
 		      const char *options);
 
 #ifdef CONFIG_SERIAL_EARLYCON
